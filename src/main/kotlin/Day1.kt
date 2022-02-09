e7caecf4b6cbe524e77fad4ba45e3e27b5a558c8
mnlcz <manuel.lopez.cosmitz@gmail.com>
2022-02-09 18:28:43 -0300

    Initial commit

---
+++ src/main/kotlin/Day1.kt
@@ -0,0 +1,43 @@
+import java.io.File
+
+object Day1
+{
+    private fun parse(): List<Int>
+    {
+        val parsed = mutableListOf<Int>()
+        val content: List<String> = File("./src/main/resources/input1.txt").readText().split("\n")
+        content.forEach { parsed.add(it.toInt())}
+        return parsed
+    }
+    object Part1
+    {
+        fun solve()
+        {
+            val input = parse()
+            var counter = 0
+            var currentMax = input.first()
+            input.forEach {
+                if(it > currentMax) counter++
+                currentMax = it
+            }
+            println("ANSWER: $counter")
+        }
+    }
+
+    object Part2
+    {
+        fun solve()
+        {
+            val input = parse()
+            var counter = 0
+            var currentMax = input.first()
+            for(i in 0..(input.size - 4))
+            {
+                if(input[i] + input[i + 1] + input[i + 2] > currentMax)
+                    counter++
+                currentMax = input[i] + input[i + 1] + input[i + 2]
+            }
+            println("ANSWER: $counter")
+        }
+    }
+}
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,5 @@
+fun main()
+{
+    Day1.Part1.solve()
+    Day1.Part2.solve()
+}
